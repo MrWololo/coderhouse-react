18425640e998ffd3e9c67790d0af5ad092347fca
MrWololo <martin_pedrosa@hotmail.com>
2021-10-16 20:09:07 -0300

    m-auto given back

--- src/container/ItemListContainer.jsx
+++ src/container/ItemListContainer.jsx
@@ -2,7 +2,7 @@ import ItemList from "../components/ItemList";
 
 const ItemListContainer = ({ greeting }) => {
   return (
-    <div className="container mx-2 text-center">
+    <div className="container mx-2 sm:m-auto text-center">
       {greeting || (
         <h2>
           A curated selection of useless items made for{" "}

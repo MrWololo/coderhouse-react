20aec104b10279aa82e3e370d7bd4026ca35259e
MrWololo <martin_pedrosa@hotmail.com>
2021-10-22 15:17:11 -0300

    Switched logic's location

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,53 +1,22 @@
-import { useEffect, useState } from "react";
-import { getItems } from "../modules/Items";
 import Item from "./Item";
 
-const ItemList = () => {
-  
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const fetchProducts = () =>
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
-        return resolve(getItems());
-      }, 2000);
-    });
-
-  useEffect(() => {
-    fetchProducts().then((newProducts) => {
-      setProducts(newProducts);
-      setIsLoading(false);
-    });
-  }, []);
-
+const ItemList = ({ products }) => {
   return (
-    <>
-      <div
-        className={`${
-          isLoading ? "flex" : "hidden"
-        } justify-center items-center m-10`}
-      >
-        <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-primary"></div>
-      </div>
-      <div className="customColumn m-2">
-        {products.map((product) => {
-          return (
-            <Item
-              key={product.id}
-              imageSRC={product.imageSRC}
-              description={product.description}
-              title={product.title}
-              id={product.id}
-              price={product.price}
-            />
-          );
-        })}
-      </div>
-    </>
+    <div className="customColumn m-2">
+      {products.map((product) => {
+        return (
+          <Item
+            key={product.id}
+            imageSRC={product.imageSRC}
+            description={product.description}
+            title={product.title}
+            id={product.id}
+            price={product.price}
+          />
+        );
+      })}
+    </div>
   );
 };
-//TODO: columns with Items from products map (page 85 from powerpoint)
-//TODO: fake promise with circularProgessIndicator, load products then show here
 
 export default ItemList;
--- src/container/ItemListContainer.jsx
+++ src/container/ItemListContainer.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
+import { getItems } from "../modules/Items";
 import ItemList from "../components/ItemList";
 
 const ItemListContainer = ({ greeting }) => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchProducts = () =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        return resolve(getItems());
+      }, 2000);
+    });
+
+  useEffect(() => {
+    fetchProducts().then((newProducts) => {
+      setProducts(newProducts);
+      setIsLoading(false);
+    });
+  }, []);
+
   return (
     <div className="container mx-2 sm:m-auto text-center">
       {greeting || (
@@ -9,8 +28,14 @@ const ItemListContainer = ({ greeting }) => {
           <span className="text-primary primary font-semibold">U</span>
         </h2>
       )}
-
-      <ItemList />
+      <div
+        className={`${
+          isLoading ? "flex" : "hidden"
+        } justify-center items-center m-10`}
+      >
+        <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-primary"></div>
+      </div>
+      <ItemList products={products} />
     </div>
   );
 };
